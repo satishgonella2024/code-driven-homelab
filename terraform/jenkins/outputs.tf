e533c736329df8e525cf5e5c2222db03eca93d07
Subrahmanya Gonella <164250924+satishgonella2024@users.noreply.github.com>
2025-01-19 11:28:40 +0000

    Add Terraform configuration for Jenkins setup

---
+++ terraform/jenkins/outputs.tf
@@ -0,0 +1,4 @@
+output "rancher_server_ip" {
+  value = regex("[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+", proxmox_vm_qemu.jenkins_server.ipconfig0)
+  description = "The IP address of the Rancher server"
+}
---
+++ terraform/jenkins/variables.tf
@@ -0,0 +1,61 @@
+variable "proxmox_host" {
+    description = "Proxmox server hostname or IP"
+}
+
+variable "proxmox_user" {
+    description = "Proxmox user"
+    default     = "root@pam"
+}
+
+variable "proxmox_password" {
+    description = "Proxmox password"
+    sensitive   = true
+}
+
+variable "proxmox_node" {
+   description = "Proxmox node name"
+}
+variable "cloud_init_template" {
+   description = "Cloud-init template name"
+    default     = "ubuntu-2404-cloud-template"
+}
+
+variable "vm_cores" {
+  description = "Number of CPU cores for VMs"
+   default     = 2
+}
+
+variable "vm_memory" {
+   description = "Memory size (in MB) for VMs"
+   default     = 2048
+}
+
+variable "vm_disk_size" {
+   description = "Disk size (in GB) for VMs"
+    default     = "20G"
+ }
+
+variable "vm_storage" {
+    description = "Storage pool for VMs"
+    default     = "workloads-storage"
+}
+
+variable "vm_gateway" {
+    description = "Gateway for the VM network"
+    default     = "192.168.4.1"
+}
+variable "cloud_init_user" {
+   description = "Default cloud-init user"
+  default     = "ubuntu"
+}
+
+variable "cloud_init_password" {
+  description = "Default cloud-init user password"
+  default     = "password"
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "Path to the SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
